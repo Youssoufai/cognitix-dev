676820226fbd26adcfd47e6be4725835711c3770
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-07-05 13:53:41 +0100

    Added Logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,13 @@
 import "./globals.css";
+
 export const metadata = {
   title: "Cognitix Development",
   description: "Official Cognitix Development Website",
+  icons: {
+    icon: '/logo.jpg',
+    shortcut: '/logo.jpg',
+    apple: '/logo.jpg',
+  },
 };
 
 export default function RootLayout({ children }) {
